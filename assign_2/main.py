7761ea3f2cbd55c41f65f9b788aa63c30a01cc27
Sourhead <58874873+SOURHEAD@users.noreply.github.com>
2023-06-26 08:05:13 -0700

    Delete assign_2 directory

--- assign_2/main.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-def request_get(url):
-    print("Sending request to ", url)
-    response = requests.get(url)                                #connects and pings the url in its given parameter and return the status code                    
-    print("Requst code recieved is", response.status_code)      #for unit tests as we need to check if the website is up,(200-299 is fine but I just used 200)
-    return response.status_code
